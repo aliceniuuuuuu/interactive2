e1d79ffc8c40236985bf463c70183b7f8dfb2e25
Alice <yufanniu@cca.edu>
2018-10-02 18:35:22 -0700

    hhhh

---
+++ p1/styles/script.js
@@ -0,0 +1,47 @@
+
+
+
+
+let scrollPosition = 0
+
+
+
+
+
+window.addEventListener('scroll',function(){
+	scrollPosition = window.scrollY
+})
+
+
+let pageOne = document.querySelector('.page1')
+let pageTwo = document.querySelector('.page2')
+let pageThree = document.querySelector('.page3')
+
+
+
+window.requestAnimationFrame(draw)
+
+function draw(){
+  
+
+	if(scrollPosition > 0 && scrollPosition < window.innerHeight){
+  	 pageOne.classList.add('lock')
+  } else {
+  	pageOne.classList.remove('lock')
+  }
+
+  if(scrollPosition > window.innerHeight && scrollPosition < window.innerHeight * 2){
+  	 pageTwo.classList.add('lock')
+  } else {
+  	pageTwo.classList.remove('lock')
+  }
+
+  if(scrollPosition > window.innerHeight * 2 && scrollPosition < window.innerHeight * 3){
+  	 pageThree.classList.add('lock')
+  } else {
+  	pageThree.classList.remove('lock')
+  }
+
+  
+  window.requestAnimationFrame(draw)
+}
